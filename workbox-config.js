780015e37251fc5fce9114572778d72c912e568a
Hugo Fache <hugofache@gmail.com>
2026-02-16 17:11:55 +0100

    Update workbox configuration to include all file types and remove exclusions for JS/CSS

--- workbox-config.js
+++ workbox-config.js
@@ -1,13 +1,14 @@
 module.exports = {
 	globDirectory: 'dist/',
 	globPatterns: [
-		'**/*.{ico,html,json,png,svg}',
-		// Exclude large JS/CSS files from precaching
-		'!**/*.js',
-		'!**/*.css'
+		'**/*.{html,js,css,ico,png,svg,json}'
+	],
+	globIgnores: [
+		'**/node_modules/**/*',
+		'sw.js',
+		'workbox-*.js'
 	],
 	swSrc: 'public/sw.js',
 	swDest: 'dist/sw.js',
-	// Increase file size limit for large bundles
 	maximumFileSizeToCacheInBytes: 10 * 1024 * 1024 // 10 MB
 };
